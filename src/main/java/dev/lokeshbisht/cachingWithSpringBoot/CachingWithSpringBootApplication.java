4a9598d0404f69563e5b63f1ae4f5b2a51093979
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-07-10 23:08:00 +0530

    Add main file

---
+++ src/main/java/dev/lokeshbisht/cachingWithSpringBoot/CachingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.lokeshbisht.cachingWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lokeshbisht/cachingWithSpringBoot/CachingWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lokeshbisht.cachingWithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
