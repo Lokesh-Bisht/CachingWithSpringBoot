0ac5175bd0c74a562f5d642ac410595771eb46e6
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-07-14 23:56:04 +0530

    Add date time format

--- src/main/java/dev/lokeshbisht/cachingWithSpringBoot/dto/instructor/InstructorDto.java
+++ src/main/java/dev/lokeshbisht/cachingWithSpringBoot/dto/instructor/InstructorDto.java
@@ -1,9 +1,12 @@
 package dev.lokeshbisht.cachingWithSpringBoot.dto.instructor;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
+
+import static dev.lokeshbisht.cachingWithSpringBoot.constants.JsonConstants.ISO8601;
 
 @Data
 public class InstructorDto {
@@ -15,12 +18,14 @@ public class InstructorDto {
 
     private Double salary;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = ISO8601)
     @JsonProperty("created_at")
     private Date createdAt;
 
     @JsonProperty("created_by")
     private String createdBy;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = ISO8601)
     @JsonProperty("updated_at")
     private Date updatedAt;
 
