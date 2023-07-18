43059328a5cdbf7b6b80b1d1e56944f29d5b73c7
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-07-18 23:57:23 +0530

    feat: #31 Cache all instructors

--- src/main/java/dev/lokeshbisht/cachingWithSpringBoot/controller/InstructorController.java
+++ src/main/java/dev/lokeshbisht/cachingWithSpringBoot/controller/InstructorController.java
@@ -5,6 +5,8 @@ import dev.lokeshbisht.cachingWithSpringBoot.dto.ApiResponseDto;
 import dev.lokeshbisht.cachingWithSpringBoot.dto.instructor.InstructorDto;
 import dev.lokeshbisht.cachingWithSpringBoot.service.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +20,13 @@ public class InstructorController {
     private InstructorService instructorService;
 
     @PostMapping("/instructor")
+    @CacheEvict(value = "instructor", key = "'all'")
     public Instructor createInstructor(@RequestBody InstructorDto instructorDto) {
         return instructorService.createInstructor(instructorDto);
     }
 
     @PutMapping("/instructor/{instructorId}")
+    @CacheEvict(value = "instructor", key = "'all'")
     public Instructor updateInstructor(@RequestBody InstructorDto instructorDto, @PathVariable Long instructorId) {
         return instructorService.updateInstructor(instructorDto, instructorId);
     }
@@ -33,11 +37,13 @@ public class InstructorController {
     }
 
     @GetMapping("/instructor/all")
+    @Cacheable(value = "instructor", key = "'all'")
     public ApiResponseDto<List<Instructor>> getAllInstructors() {
         return instructorService.getAllInstructors();
     }
 
     @DeleteMapping("/instructor/{instructorId}")
+    @CacheEvict(value = "instructor", key = "'all'")
     public void deleteInstructor(@PathVariable Long instructorId) {
         instructorService.deleteInstructor(instructorId);
     }
