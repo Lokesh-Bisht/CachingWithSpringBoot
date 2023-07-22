046b08b439bbe9b69cd280bd0f411363483d31c9
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-07-22 23:34:58 +0530

    Store subjectId counter in cache

--- src/main/java/dev/lokeshbisht/cachingWithSpringBoot/mapper/SubjectMapper.java
+++ src/main/java/dev/lokeshbisht/cachingWithSpringBoot/mapper/SubjectMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface SubjectMapper {
 
-    Subject toSubject(SubjectDto subjectDto);
+    Subject toSubject(SubjectDto subjectDto, Long subjectId);
 }
