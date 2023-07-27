8b23b10e04e3a4ce7c8538d5e8bc89c76ac10024
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-07-27 20:29:18 +0530

    Find department by departmentId

--- src/main/java/dev/lokeshbisht/cachingWithSpringBoot/repository/DepartmentRepository.java
+++ src/main/java/dev/lokeshbisht/cachingWithSpringBoot/repository/DepartmentRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends MongoRepository<Department, String> {
+
+    Department findOneByDepartmentId(Long departmentId);
 }
